91c048e370e55884900cf4ea14014ba603808ca7
fhawas87 <jakubwozniak775@gmail.com>
2026-01-05 21:32:04 +0100

    Delete organizm directory

--- organizm/roslina/gatunki/guarana.cpp
+++
@@ -1,9 +0,0 @@
-#include "gatunki_r.h"
-#include "swiat.h"
-
-void Guarana::kolizja(Organizm* inny) {
-
-	swiat.przenies_organizm(inny, this->x, this->y);
-	swiat.usun_organizm(this);
-	inny->inc_sila(3);
-}
